9957a578f07f0b372c9ea3968965ec4c67aaf7a7
snabu <samuli.tuoriniemi@yahoo.com>
2018-04-07 17:40:30 +0300

    exercice 4

--- osa2/app1/src/index.js
+++ osa2/app1/src/index.js
@@ -2,33 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10,
-                id: 1
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7,
-                id: 2
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14,
-                id: 3
-            }
-        ]
-    }
+    const kurssit = [
+        {
+            nimi: 'Half Stack -sovelluskehitys',
+            id: 1,
+            osat: [
+                {
+                    nimi: 'Reactin perusteet',
+                    tehtavia: 10,
+                    id: 1
+                },
+                {
+                    nimi: 'Tiedonvälitys propseilla',
+                    tehtavia: 7,
+                    id: 2
+                },
+                {
+                    nimi: 'Komponenttien tila',
+                    tehtavia: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            nimi: 'Node.js',
+            id: 2,
+            osat: [
+                {
+                    nimi: 'Routing',
+                    tehtavia: 3,
+                    id: 1
+                },
+                {
+                    nimi: 'Middlewaret',
+                    tehtavia: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Kurssi kurssi={kurssi} />
+            <h1>Opetusohjelma</h1>
+            <Kurssit kurssit = {kurssit} />
         </div>
     )
+}
 
+const Kurssit = ({kurssit}) => {
+    return (
+
+        kurssit.map(kurssi =>
+            <div key={kurssi.id}>
+                <Kurssi kurssi={kurssi} />
+            </div>
+        ))
 }
 
 
