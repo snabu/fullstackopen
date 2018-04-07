7a89da63366e570eca7d03f6292f551020cfcf08
snabu <samuli.tuoriniemi@yahoo.com>
2018-04-07 16:33:44 +0300

    exercice 1

---
+++ osa2/app1/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10,
+                id: 1
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7,
+                id: 2
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14,
+                id: 3
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Kurssi kurssi={kurssi} />
+        </div>
+    )
+
+}
+
+
+const Kurssi = ({kurssi}) => {
+    console.log("Kurssi on ", kurssi)
+    
+    const Otsikko =({otsikko}) => {
+        return(<h1>{otsikko}</h1>)
+    }
+
+    const Sisalto = ({osat}) => {
+        return(osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>))
+    }
+
+    return(
+        <div>
+        <Otsikko otsikko={kurssi.nimi}/>
+        <Sisalto osat={kurssi.osat}/>
+        </div>
+    )
+}
+
+
+ReactDOM.render(
+    <App  />,
+    document.getElementById('root')
+)
