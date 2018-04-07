bf142ea16f48fc0d7b6134227fa51fb634c42ac4
snabu <samuli.tuoriniemi@yahoo.com>
2018-04-07 17:19:52 +0300

    exercice 2, 3

--- osa2/app1/src/index.js
+++ osa2/app1/src/index.js
@@ -34,7 +34,7 @@ const App = () => {
 
 const Kurssi = ({kurssi}) => {
     console.log("Kurssi on ", kurssi)
-    
+
     const Otsikko =({otsikko}) => {
         return(<h1>{otsikko}</h1>)
     }
@@ -43,10 +43,20 @@ const Kurssi = ({kurssi}) => {
         return(osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>))
     }
 
+    const Yhteensa = ({osat}) => {
+        console.log(osat)
+        const count = osat.map(osa => osa.tehtavia).reduce((acc,cur) => acc+cur)
+        console.log("count is ", count)
+        return(<p>yhteensä {count} tehtävää</p>)
+    }
+
+
     return(
         <div>
         <Otsikko otsikko={kurssi.nimi}/>
         <Sisalto osat={kurssi.osat}/>
+        <Yhteensa osat={kurssi.osat}/>
+
         </div>
     )
 }
