25a8d1494246d8b7e9b2233b5b45ed71322fcc41
unknown <samuli.tuoriniemi@yahoo.com>
2018-10-20 15:13:36 +0300

    excercise 3, pt4

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,12 +4,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const mongoUrl = 'mongodb://' + process.env.FULLSTACKOPEN_MLAB_USER + ':' + process.env.FULLSTACKOPEN_MLAB_PWD + '@ds239309.mlab.com:39309/fullstackopen'
 
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
+
+
+mongoose.connect(mongoUrl)
 
 const PORT = 3003
 app.listen(PORT, () => {
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = 'mongodb://' + process.env.FULLSTACKOPEN_MLAB_USER + ':' + process.env.FULLSTACKOPEN_MLAB_PWD + '@ds239309.mlab.com:39309/fullstackopen'
-
-mongoose.connect(mongoUrl)
-
-
 const Blog = mongoose.model('Blog', {
     title: String,
     author: String,
