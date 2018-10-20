40f9384964d60717ea0ee557322c6b8d3d992567
unknown <samuli.tuoriniemi@yahoo.com>
2018-10-21 00:19:20 +0300

    excercise 7, pt4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -41,6 +41,28 @@ const mostBlogs = (blogs) => {
     return {author : blogs[index].author, blogs : most}
 }
 
+const mostLikes = (blogs) => {
+    if (blogs.length == 0)
+        return({})
+    let count = 0
+    let most = 0
+    let index = -1
+    for (let i = 0; i < blogs.length; i++) {
+        for (let j = i; j < blogs.length; j++) {
+            if (blogs[i].author == blogs[j].author)
+                count += blogs[j].likes
+            if (count > most) {
+                most = count
+                index = i
+            }
+        }
+        count = 0
+    }
+
+    return {author : blogs[index].author, likes : most}
+}
+
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
