8da9f34aa039dd36f77da5b344e71f35ff8d7b57
unknown <samuli.tuoriniemi@yahoo.com>
2018-10-20 22:19:23 +0300

    excercise 4, pt4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
    return 1
 }
 
+
+
+const reducer = (acc,obj) => {
+    return acc + obj.likes
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
